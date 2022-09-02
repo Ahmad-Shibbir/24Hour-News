5765882957472441b2ecbb8948c5b910cd388d0f
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-09-03 00:27:17 +0800

    load news catagories name

--- Script/24HourNews.js
+++ Script/24HourNews.js
@@ -1,1 +1,14 @@
-console.log("inside 24 hour news js file");
+const LoadNewsCatagory = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => newsCategory(data.data.news_category));
+}
+
+LoadNewsCatagory();
+const newsCategory = catagories =>{
+
+    
+    catagories.forEach(catagory => {
+       console.log(catagory.category_name) 
+   
+})}
