625f399b88f47f85d519fb3a98c9c13389fd8fe4
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-09-03 00:53:42 +0800

    loded news category displayed

--- Script/24HourNews.js
+++ Script/24HourNews.js
@@ -9,6 +9,14 @@ const newsCategory = catagories =>{
 
     
     catagories.forEach(catagory => {
+        const newsCategorySection = document.getElementById("news-catagories");
+        const newsCategoryDiv = document.createElement('div');
+        newsCategoryDiv.classList.add('inline')
+        newsCategoryDiv.innerHTML=`
+
+        <button class="btn btn-ghost ">${catagory.category_name}</button>
+        `
+        newsCategorySection.appendChild(newsCategoryDiv);
        console.log(catagory.category_name) 
    
 })}
