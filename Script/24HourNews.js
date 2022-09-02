c1a78bc6a437294183accf89d6c568f3f5b1848f
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-09-03 01:53:29 +0800

    load data for every single catagory

--- Script/24HourNews.js
+++ Script/24HourNews.js
@@ -13,10 +13,26 @@ const newsCategory = catagories =>{
         const newsCategoryDiv = document.createElement('div');
         newsCategoryDiv.classList.add('inline')
         newsCategoryDiv.innerHTML=`
-
-        <button class="btn btn-ghost ">${catagory.category_name}</button>
+        <button onclick="loadSingleCatagoryNews(${catagory.category_id})" class="btn btn-ghost ">${catagory.category_name}</button>
         `
         newsCategorySection.appendChild(newsCategoryDiv);
-       console.log(catagory.category_name) 
+        console.log(catagory.category_name) 
    
 })}
+
+function loadSingleCatagoryNews(categoryId){
+    fetch(`https://openapi.programming-hero.com/api/news/category/0${categoryId}`)
+    // fetch(`https://openapi.programming-hero.com/api/news/category/01`)
+    .then(res => res.json())
+    .then(data => displayNewse(data.data));
+    console.log(categoryId,'helooooooooooooooooooooooooooooo')
+}
+
+const displayNewse = newsForSingleCategories =>{
+    newsForSingleCategories.forEach(newsForSingleCategorie => {
+        const {author, details ,image_url,rating }= newsForSingleCategorie;
+
+        console.log(details,author,image_url)
+    });
+   
+}
