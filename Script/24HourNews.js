a3f64e2443371f729551364940a7e75b032dd1df
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-09-03 08:15:32 +0800

    added section for display how much number of item found for each category

--- Script/24HourNews.js
+++ Script/24HourNews.js
@@ -8,6 +8,7 @@ LoadNewsCatagory();
 const newsCategory = catagories =>{
 
     const newsCategorySection = document.getElementById("news-catagories");
+    
     catagories.forEach(catagory => {
         
         const newsCategoryDiv = document.createElement('div');
@@ -17,30 +18,45 @@ const newsCategory = catagories =>{
         `
         newsCategorySection.appendChild(newsCategoryDiv);
         console.log(catagory.category_name) 
+
+        // var catagoryName = catagory.category_name;
    
 })}
-
+loadSingleCatagoryNews(2);
 function loadSingleCatagoryNews(categoryId){
     fetch(`https://openapi.programming-hero.com/api/news/category/0${categoryId}`)
     // fetch(`https://openapi.programming-hero.com/api/news/category/01`)
     .then(res => res.json())
     .then(data => displayNewse(data.data));
-    console.log(categoryId,'helooooooooooooooooooooooooooooo')
+    
 }
 
 const displayNewse = newsForSingleCategories =>{
+
+    
+    const numberOfNews  = document.getElementById('number-of-news')
+    const DivForNumberOFNews = document.createElement('div')
     const newsCard = document.getElementById('news-card');
     newsCard.innerHTML ='';
-    newsForSingleCategories.forEach(newsForSingleCategorie => {
 
-        const {author, details ,thumbnail_url,total_view,title,_id }= newsForSingleCategorie;        
+   
+
+    newsForSingleCategories.forEach(newsForSingleCategorie => {
+        const {author, details ,thumbnail_url,total_view,title,_id }= newsForSingleCategorie;  
+        DivForNumberOFNews.innerHTML =`
+        <p class="font-bold text-2xl bg-white text-black border border-sky-500 m-10 p-4 rounded-lg">${newsForSingleCategories.length} items found for this category </p>
+        `
+        numberOfNews.appendChild(DivForNumberOFNews)
+
+
+             
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML = `
       <div class="gid grid-cols-2 card card-side bg-base-100 shadow-xl mt-5">
       <figure class="col-span-2"><img class="h-80 w-72" src="${thumbnail_url}" alt="Movie"></figure>
       <div class="card-body col-span-2">
         <h2 class="card-title">${title}</h2>
-        <p>${details}</p>
+        <p>${details.slice(0,400)}...</p>
         <div>
           <div class="grid grid-cols-3 items-center gap-8 font-bold">
             <div id="author" class="flex gap-2">
